93a2bb173b292c47d0664fd334802f5104bef79e
Himanshu <himanshusingh2389@gmail.com>
2025-10-04 10:37:17 +0530

    Funtional Interface Part done

--- java_8_features/src/functional_interface/MyFunctionalInterface.java
+++ java_8_features/src/functional_interface/MyFunctionalInterface.java
@@ -3,6 +3,7 @@ package functional_interface;
 // Creating a Functional Interface
 @FunctionalInterface
 interface MyFunctionalInterface {
+    //Show Message
     void showMessage(String message);
 }
 
